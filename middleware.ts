446ab4f0c5121712a594adea8a92e460cc995bc5
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2026-02-25 09:56:53 -0300

    fix: middleware on iOS

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,24 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
-import { auth } from "@/lib/auth"
+// import { NextResponse } from "next/server"
+// import type { NextRequest } from "next/server"
+// import { auth } from "@/lib/auth"
 
-export async function middleware(req: NextRequest) {
-  const session = await auth()
+// export async function middleware(req: NextRequest) {
+//   const session = await auth()
 
-  if (!session) {
-    return NextResponse.redirect(new URL("/login", req.url))
-  }
+//   if (!session) {
+//     return NextResponse.redirect(new URL("/login", req.url))
+//   }
 
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
+
+// export const config = {
+//   matcher: [
+//     "/admin-dashboard/:path*",
+//   ],
+// }
 
 export const config = {
-  matcher: [
-    "/admin-dashboard/:path*",
-  ],
+  matcher: ["/admin-dashboard/:path*"],
 }
