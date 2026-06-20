5be157455b46303284800fa40a6dc1c9c9d7a3b5
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2026-06-20 18:46:59 -0300

    fix: prisma transaction time out increase

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,6 +8,7 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: [],
+    transactionTimeout: 30000,
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
